acf6fb9af1aeda0ac559fbeb0ea6dda87a2b4777
Utkarsh Sharma <ussharma7696@gmail.com>
2024-02-11 18:07:42 +0530

    user registration

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,10 +2,23 @@ import {Router} from "express"
 import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
+import {upload} from "../middlewares/multor.middleware.js"
 
 
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name : "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
 
-router.route("/register").post(registerUser)
 // router.route("/login").post(login)
 
 
