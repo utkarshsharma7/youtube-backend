fb7cddaa2a88bf15351eb96876e3f956dea23734
Utkarsh Sharma <ussharma7696@gmail.com>
2024-02-26 03:33:32 +0530

    Subscription routes updated

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -55,85 +55,161 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 })
 
 // controller to return subscriber list of a channel
+// controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    if (!channelId || !isValidObjectId(channelId)) {
-      throw new ApiError(400, 'Invalid Channel ID')
+    let { channelId } = req.params;
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channelId");
     }
+
+    channelId = new mongoose.Types.ObjectId(channelId);
+
     const subscribers = await Subscription.aggregate([
-      {
-        $match: {
-          channel: new mongoose.Types.ObjectId(channelId),
+        {
+            $match: {
+                channel: channelId,
+            },
         },
-      },
-      {
-        $lookup: {
-          from: "users",
-          localField: "subscriber",
-          foreignField: "_id",
-          as: "subscribers",
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriber",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "subscriptions",
+                            localField: "_id",
+                            foreignField: "channel",
+                            as: "subscribedToSubscriber",
+                        },
+                    },
+                    {
+                        $addFields: {
+                            subscribedToSubscriber: {
+                                $cond: {
+                                    if: {
+                                        $in: [
+                                            channelId,
+                                            "$subscribedToSubscriber.subscriber",
+                                        ],
+                                    },
+                                    then: true,
+                                    else: false,
+                                },
+                            },
+                            subscribersCount: {
+                                $size: "$subscribedToSubscriber",
+                            },
+                        },
+                    },
+                ],
+            },
         },
-      },
-      {
-        $project: {
-          _id: 1,
-          channel: 1,
-          subscriber: 1,
-          createdAt: 1,
-          updatedAt: 1,
-          subscribers: {
-            $arrayElemAt: ["$subscribers", 0],
-          },
+        {
+            $unwind: "$subscriber",
         },
-      },
-      {
-        $project: {
-          _id: 1,
-          subscriber: 1,
-          channel: 1,
-          subscribers: {
-            username: 1,
-            avatar: 1,
-          },
-          createdAt: 1,
-          updatedAt: 1,
+        {
+            $project: {
+                _id: 0,
+                subscriber: {
+                    _id: 1,
+                    username: 1,
+                    fullName: 1,
+                    "avatar.url": 1,
+                    subscribedToSubscriber: 1,
+                    subscribersCount: 1,
+                },
+            },
         },
-      },
     ]);
 
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                subscribers,
+                "subscribers fetched successfully"
+            )
+        );
+});
 
-
-if (!subscribers) {
-  throw new ApiError(404, "The channel does not exist");
-}
-
-return res.status(200).json(
-  new ApiResponse(
-    200,
-    {
-      numOfSubscribers: subscribers.length,
-      subscribers,
-    },
-    "Creating a function to get a channel's subscriber"
-  )
-);
-})
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
+  const { subscriberId } = req.params;
 
+  const subscribedChannels = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: new mongoose.Types.ObjectId(subscriberId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "subscribedChannel",
+        pipeline: [
+          {
+            $lookup: {
+              from: "videos",
+              localField: "_id",
+              foreignField: "owner",
+              as: "videos",
+            },
+          },
+          {
+            $addFields: {
+              latestVideo: {
+                $last: "$videos",
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$subscribedChannel",
+    },
+    {
+      $project: {
+        _id: 0,
+        subscribedChannel: {
+          _id: 1,
+          username: 1,
+          fullName: 1,
+          "avatar.url": 1,
+          latestVideo: {
+            _id: 1,
+            "videoFile.url": 1,
+            "thumbnail.url": 1,
+            owner: 1,
+            title: 1,
+            description: 1,
+            duration: 1,
+            createdAt: 1,
+            views: 1,
+          },
+        },
+      },
+    },
+  ]);
 
-    if(!isValidObjectId(subscriberId)){
-        throw new ApiError(400, "Invalid subscriber Id")
-    }
-    const  subscribedChannels = await Subscription.find({subscriber: subscriberId}, 'channel').sort('-createdAt');
-
-    return res
-    .res(200)
-    .json(new ApiResponse(200, subscribedChannels, "Subscribed channels fetched successfully"))
-})
-
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        subscribedChannels,
+        "subscribed channels fetched successfully"
+      )
+    );
+});
 export {
     toggleSubscription,
     getUserChannelSubscribers,
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -10,10 +10,10 @@ const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router
-    .route("/c/:channelId")
-    .get(getSubscribedChannels)
-    .post(toggleSubscription);
+  .route("/c/:channelId")
+  .get(getUserChannelSubscribers)
+  .post(toggleSubscription);
 
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+router.route("/u/:subscriberId").get(getSubscribedChannels);
 
 export default router
